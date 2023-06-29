bb996637bc655a776830e5943440e8053c951638
ayano_kondo@mail.toyota.co.jp <aya-no>
2023-06-29 22:09:03 +0900

    レシピIDからレシピ取得API作成

--- testamplify/src/App.js
+++ testamplify/src/App.js
@@ -7,13 +7,16 @@ import { withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 
 import awsExports from './aws-exports';
-import { createRecipeAPI, fetchRecipeAPI, deleteRecipeAPI, dataClearAPI, createRecipeItemAPI } from './api';
+import { createRecipeAPI, fetchRecipeAPI, fetchIdRecipeAPI, deleteRecipeAPI, dataClearAPI, createRecipeItemAPI, fetchRecipeItemAPI } from './api';
 Amplify.configure(awsExports);
 
 function App({ user }) {
   const [recipe, setRecipe] = useState([]);
+  const [recipeItem, setRecipeItem] = useState([]);
+  const [recipeId, setRecipeId] = useState([]);
+
   const [inputValue, setInputValue] = useState('');
-  const [deleteInputValue, setDaleteInputValue] = useState("");
+  const [idInputValue, setIdInputValue] = useState("");
 
   useEffect(() => {
     // レシピの一覧データの取得
@@ -22,6 +25,12 @@ function App({ user }) {
       setRecipe(JSON.parse(data))
     }
     fetchRecipeList();
+    // レシピ材料の一覧データの取得
+    const fetchRecipeItemList = async () => {
+      const data = await fetchRecipeItemAPI()
+      setRecipeItem(JSON.parse(data))
+    }
+    fetchRecipeItemList();
   }, [])
 
   // レシピ名登録ようインプットフォームが変更されたらsetInputValueを変更
@@ -47,23 +56,31 @@ function App({ user }) {
 
   // レシピIDを入力したらSet DaleteInputValueを変更
   const DeleteInputValueOnChange = (value) => {
-    setDaleteInputValue(value)
+    setIdInputValue(value)
   }
 
   // レシピの削除
   async function deleteRecipe(data) {
     await deleteRecipeAPI(data);
-    setDaleteInputValue("");
+    // setIdInputValue("");
   }
 
-
+  // レシピの取得（id）指定
+  async function fetchIdRecipe(id) {
+    const recipe = await fetchIdRecipeAPI(id);
+    const idRecipe = JSON.parse(recipe[0])
+    console.log(idRecipe)
+    setRecipeId(idRecipe);
+    // setIdInputValue("");
+  }
 
   // レシピ材料の登録
   async function sendRecipeItem() {
     const data = {
-      recipeItem: "牛肉" + user.attributes.sub,
-      quantity: 200,
-      corner: "肉"
+      recipeItem: "にんじん" + user.attributes.sub,
+      quantity: 2,
+      corner: "野菜",
+      unit: "個"
     }
     // レシピ材料を登録するAPI呼び出し
     await createRecipeItemAPI(data)
@@ -81,18 +98,31 @@ function App({ user }) {
 
   return (
     <>
-      <h1>Hello {user.attributes.email} id{user.attributes.sub}</h1>
+      <h1>Hello {user.attributes.email} </h1>
+      <h3>ログインユーザー：{user.attributes.sub}</h3>
       <button onClick={signOut}>Sign out</button>
 
       {/* {console.log(user)} */}
 
-      <h2>レシピの一覧の取得↓</h2>
-
+      <h2>レシピ一覧の取得↓</h2>
       {recipe && recipe.map((recipe, index) => {
         return (
           <div key={index}>
-            {/* {console.log(recipe)} */}
-            {recipe.recipe}  id:{recipe.id} 登録ユーザー:{recipe.owner}
+            {/* {console.log("recipe;", recipe)} */}
+            ⭐️レシピ名 : {recipe.recipe}
+            ⭐️id : {recipe.id}
+            ⭐️RecipeItems : {recipeItem.RecipeItems}
+          </div>
+        )
+      })}
+      <h2>レシピ材料一覧の取得↓</h2>
+      {recipeItem && recipeItem.map((recipeItem, index) => {
+        return (
+          <div key={index}>
+            {/* {console.log("recipeItem:", recipeItem)} */}
+            ⭐️レシピ材料名 : {recipeItem.recipeItem}
+            ⭐️id : {recipeItem.id}
+            ⭐️recipeID : {recipeItem.recipeID}
           </div>
         )
       })}
@@ -101,9 +131,12 @@ function App({ user }) {
       <button onClick={() => sendRecipe(inputValue)}>レシピ名を登録</button>
       <h2>レシピの材料登録↓</h2>
       <button onClick={() => sendRecipeItem()}>レシピダミー材料を登録</button>
-      <h2>レシピの削除（レシピIDを入力して削除</h2>
-      <input type='text' value={deleteInputValue} onChange={(e) => DeleteInputValueOnChange(e.target.value)} />
-      <button onClick={() => deleteRecipe(deleteInputValue)}>レシピを削除</button>
+      <h2>IDを入力してレシピの削除・取得）</h2>
+      <input type='text' value={idInputValue} onChange={(e) => DeleteInputValueOnChange(e.target.value)} />
+      <button onClick={() => deleteRecipe(idInputValue)}>レシピを削除</button>
+      <button onClick={() => fetchIdRecipe(idInputValue)}>レシピを取得</button>
+      <h3>IDから取得したレシピ</h3>
+      {recipeId}
     </ >
   );
 }
--- testamplify/src/api.js
+++ testamplify/src/api.js
@@ -31,24 +31,26 @@ export const createRecipeAPI = async (data) => {
     }
 };
 
-// レシピItemの作成
+// レシピ材料の作成
 export const createRecipeItemAPI = async (data) => {
-    const { recipeItem, quantity, corner } = data;
+    const { recipeItem, quantity, corner, unit } = data;
     try {
         await DataStore.save(
             new RecipeItem({
                 recipeItem,
                 quantity,
-                corner
+                corner,
+                unit,
             })
         );
+        console.log("レシピ材料登録完了")
     } catch (err) {
         throw err;
     }
 };
 
 
-// レシピの取得
+// レシピ一覧の取得
 export const fetchRecipeAPI = async () => {
     try {
         const recipeList = await DataStore.query(Recipe);
@@ -57,6 +59,33 @@ export const fetchRecipeAPI = async () => {
         throw err;
     }
 }
+
+// レシピの取得（id)指定
+export const fetchIdRecipeAPI = async (id) => {
+    try {
+        const recipe = await DataStore.query(Recipe, r => r.id.eq(id));
+        console.log("recipe:", JSON.stringify(recipe[0], null, 2))
+        // let item;
+
+        // const recipeItem = recipeList[0].RecipeItems.values.then(item => item = JSON.stringify(item, null, 2))
+        // console.log(recipeItem)
+
+        return JSON.stringify(recipe[0], null, 2);
+    } catch (err) {
+        throw err;
+    }
+}
+
+// レシピ材料一覧の取得
+export const fetchRecipeItemAPI = async () => {
+    try {
+        const recipeList = await DataStore.query(RecipeItem);
+        return JSON.stringify(recipeList, null, 2);
+    } catch (err) {
+        throw err;
+    }
+}
+
 
 // レシピの削除
 export const deleteRecipeAPI = async (id) => {
